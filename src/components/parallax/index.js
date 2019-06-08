09e1ae94dd04aa557a90e4134077b06f9d541ea4
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-09 02:20:57 +0500

    Update: :construction: Parallax -- medium  3 is Modified :  1 : package.json  2 : src/components/parallax/index.js  3 : yarn.lock

--- src/components/parallax/index.js
+++ src/components/parallax/index.js
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Rellax from "rellax";
 
-const Parallax = () => {
-  return (
-    <section className="home_banner_area">
-      <div className="banner_inner">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-7">
-              <div className="banner_content">
-                <h3 className="text-uppercase">Hell0</h3>
-                <h1 className="text-uppercase">I am rahi satner</h1>
-                <h5 className="text-uppercase">senior wordpress developer</h5>
-                <div className="d-flex align-items-center">
-                  <a
-                    className="primary_btn"
-                    href="https://www.upwork.com/o/profiles/users/_~014f12876e410ed5e2/"
+class Parallax extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    new Rellax(".rellax");
+  }
+  render() {
+    return (
+      <section className="home_banner_area">
+        <div className="banner_inner">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7">
+                <div className="banner_content">
+                  <h3
+                    className="text-uppercase rellax"
+                    data-rellax-speed="-1.1"
                   >
-                    <span>Hire Me</span>
-                  </a>
-                  <Link className="primary_btn tr-bg" to="/CV">
-                    <span>Get CV</span>
-                  </Link>
+                    Hell0
+                  </h3>
+                  <h1 className="text-uppercase">I am hams ahmed ansari</h1>
+                  <h5 className="text-uppercase rellax" data-rellax-speed="1.1">
+                    full stack mern developer
+                  </h5>
+                  <div
+                    className="d-flex align-items-center rellax"
+                    data-rellax-speed="1.1"
+                    data-rellax-percentage="0.5"
+                  >
+                    <a
+                      className="primary_btn"
+                      href="https://www.upwork.com/o/profiles/users/_~014f12876e410ed5e2/"
+                    >
+                      <span>Hire Me</span>
+                    </a>
+                    <Link className="primary_btn tr-bg" to="/CV">
+                      <span>Get CV</span>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-5">
-              <div className="home_right_img">
-                <img className="" src="img/banner/home-right.png" alt="" />
+              <div className="col-lg-5">
+                <div className="home_right_img">
+                  <img
+                    className="rellax"
+                    data-rellax-speed="1.5"
+                    data-rellax-percentage="0.5"
+                    src="img/banner/home-right.png"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
-
+      </section>
+    );
+  }
+}
 export default Parallax;
