83d5d2aa69f647627f44f5fa601dc9e3667805fc
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-09 01:49:07 +0500

    Update: :construction: Header and Footer  1 is Modified :  1 : src/Layout/footer/index.js

--- src/Layout/footer/index.js
+++ src/Layout/footer/index.js
@@ -1,43 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class="footer_area">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-12">
-            <div class="footer_top flex-column">
-              <div class="footer_logo">
-                <a href="#">
+    <footer className="footer_area">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-12">
+            <div className="footer_top flex-column">
+              <div className="footer_logo">
+                <Link to="/">
                   <img src="img/logo.png" alt="" />
-                </a>
+                </Link>
                 <h4>Follow Me</h4>
               </div>
-              <div class="footer_social">
-                <a href="#">
-                  <i class="fa fa-facebook" />
+              <div className="footer_social">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/hamsahmedansari"
+                >
+                  <i className="fa fa-facebook" />
                 </a>
-                <a href="#">
-                  <i class="fa fa-twitter" />
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://twitter.com/hamsahmedansari"
+                >
+                  <i className="fa fa-twitter" />
                 </a>
-                <a href="#">
-                  <i class="fa fa-dribbble" />
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/hamsahmedansari"
+                >
+                  <i className="fa fa-github" />
                 </a>
-                <a href="#">
-                  <i class="fa fa-behance" />
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.behance.net/hamsahmeda1813"
+                >
+                  <i className="fa fa-behance" />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/hamsahmedansari/"
+                >
+                  <i className="fa fa-linkedin" />
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="row footer_bottom justify-content-center">
-          <p class="col-lg-8 col-sm-12 footer-text">
+        <div className="row footer_bottom justify-content-center">
+          <p className="col-lg-8 col-sm-12 footer-text">
             Copyright &copy;{new Date().getFullYear()} All rights reserved |
-            This template is made with
-            <i class="fa fa-heart-o" aria-hidden="true" /> by
-            <a href="https://colorlib.com" target="_blank">
+            This template is made with{" "}
+            <a
+              href="https://colorlib.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Colorlib
             </a>
+          </p>
+          <p className="col-lg-8 col-sm-12 footer-text">
+            Currently i am working on my own portfolio Hopefully Lunched in few
+            weeks... <i className="fa fa-heart" />
           </p>
         </div>
       </div>
