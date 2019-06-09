8862ecd6941a5c9914bc9f200f5859686830ded5
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-09 22:13:45 +0500

    Update: :construction: About us  1 is Modified :  1 : src/Layout/routes/index.js  2 is Created :  1 : src/components/banner/  2 : src/views/about/

--- src/Layout/routes/index.js
+++ src/Layout/routes/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import home from "../../views/home";
+import About from "../../views/about";
 
 const Routing = () => {
   return (
     <Switch>
-      <Route path="/about" exact component={home} />
+      <Route path="/about" exact component={About} />
       <Route path="/skill" exact component={home} />
       <Route path="/portfolio" exact component={home} />
       <Route path="/contact" exact component={home} />
