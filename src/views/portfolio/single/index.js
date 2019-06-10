a248b7b745eba9d273155bc769438ffbe94ffd13
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-11 01:46:05 +0500

    Update: :construction: Protfolio - Single  1 is Modified :  1 : src/views/portfolio/single/index.js

--- src/views/portfolio/single/index.js
+++ src/views/portfolio/single/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Banner from "../../../components/banner";
 import data from "../../../data/portfolio";
 
 const SinglePortfolio = props => {
-  //   const { id } = props;
   const id = 1;
   let temp = {};
   data.find(d =>
@@ -16,9 +14,6 @@ const SinglePortfolio = props => {
     })
   );
   const starts = Array.apply(null, new Array(temp.rating));
-  console.log("====================================");
-  console.log(starts);
-  console.log("====================================");
   return (
     <React.Fragment>
       <Banner
@@ -28,24 +23,24 @@ const SinglePortfolio = props => {
         ]}
         title={temp.title}
       />
-      <section class="portfolio_details_area mt-5">
-        <div class="container">
-          <div class="portfolio_details_inner">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="left_img">
-                  <img class="img-fluid" src={temp.image} alt="" />
+      <section className="portfolio_details_area mt-5">
+        <div className="container">
+          <div className="portfolio_details_inner">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="left_img">
+                  <img className="img-fluid" src={temp.image} alt="" />
                 </div>
               </div>
-              <div class="offset-lg-1 col-lg-5">
-                <div class="portfolio_right_text mt-30">
-                  <h4 class="text-uppercase">{temp.title}</h4>
+              <div className="offset-lg-1 col-lg-5">
+                <div className="portfolio_right_text mt-30">
+                  <h4 className="text-uppercase">{temp.title}</h4>
                   <p>{temp.description}</p>
-                  <ul class="list">
+                  <ul className="list">
                     <li>
                       <span>Rating</span>:
-                      {starts.map(star => (
-                        <i class="fa fa-star" />
+                      {starts.map((star, i) => (
+                        <i className="fa fa-star" key={i} />
                       ))}
                     </li>
                     <li>
