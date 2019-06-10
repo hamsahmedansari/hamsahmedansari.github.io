dab723ea24f01794c2b7110be92b27886dc23e7b
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-11 00:50:49 +0500

    Update: :construction: Protfolio - Single  1 is Modified :  1 : src/Layout/routes/index.js  1 is Created :  1 : src/views/portfolio/single/

--- src/Layout/routes/index.js
+++ src/Layout/routes/index.js
@@ -6,12 +6,14 @@ import Skills from "../../views/skills";
 import Portfolio from "../../views/portfolio";
 import Contact from "../../views/contact";
 import error from "../../views/404";
+import SinglePortfolio from "../../views/portfolio/single";
 
 const Routing = () => {
   return (
     <Switch>
       <Route path="/about" exact component={About} />
       <Route path="/skill" exact component={Skills} />
+      <Route path="/projects" component={SinglePortfolio} />
       <Route path="/portfolio" exact component={Portfolio} />
       <Route path="/contact" exact component={Contact} />
       <Route path="/404" exact component={error} />
