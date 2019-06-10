acddc540b18065821b1fe802f9643c12677735ee
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-11 00:41:19 +0500

    Update: :construction: Contact  1 is Modified :  1 : src/views/contact/index.js

--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -3,7 +3,39 @@ import Banner from "../../components/banner";
 
 const Contact = props => {
   return (
-    <Banner pages={[{ name: "Contact", link: "/contact" }]} title="Contact" />
+    <React.Fragment>
+      <Banner pages={[{ name: "Contact", link: "/contact" }]} title="Contact" />
+      <section class="contact_area section_gap">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="d-flex justify-content-between contact_info">
+                <div class="info_item">
+                  <i class="lnr lnr-home" />
+                  <h6>Karachi, Pakistan</h6>
+                </div>
+                <div class="info_item">
+                  <i class="lnr lnr-phone-handset" />
+                  <h6>
+                    <a href="tel:+923402049917">+92 (340) 2049 917</a>
+                  </h6>
+                  <p>24/7 Available</p>
+                </div>
+                <div class="info_item">
+                  <i class="lnr lnr-envelope" />
+                  <h6>
+                    <a href="mailto:hams.ahmed.ansari95@gmail.com">
+                      hams.ahmed.ansari95@gmail.com
+                    </a>
+                  </h6>
+                  <p>Send us your query anytime!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
