1c01dd54d63d4940ab2b254f3fa230c11dbef024
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-13 00:19:41 +0500

    Updates

---
+++ precache-manifest.19a2fd4d662537be0d128ea8655204f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3d88382a6c0440f0a89ac54d399c5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734d231959a695040781",
+    "url": "/static/css/2.60f2038a.chunk.css"
+  },
+  {
+    "revision": "18bb75dac1df55ec57e2",
+    "url": "/static/css/main.8d1ad632.chunk.css"
+  },
+  {
+    "revision": "734d231959a695040781",
+    "url": "/static/js/2.fc6102f9.chunk.js"
+  },
+  {
+    "revision": "18bb75dac1df55ec57e2",
+    "url": "/static/js/main.fe0a22f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f31b5bcda076125bf7010c781a4578a0",
+    "url": "/static/media/404.f31b5bcd.gif"
+  }
+]);
